7584d61101c1ff223928715366cd4f2187ffc2d1
Sergey Kalinovsky <SergeyKalinovsky@coherentsolutions.com>
2018-09-28 15:45:50 +0300

    Migrated to AndroidX

--- app/src/main/java/com/criticalgnome/coroutitesdemo/MainAdapter.kt
+++ app/src/main/java/com/criticalgnome/coroutitesdemo/MainAdapter.kt
@@ -1,6 +1,6 @@
 package com.criticalgnome.coroutitesdemo
 
-import android.support.v7.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +8,12 @@ import com.criticalgnome.coroutitesdemo.model.Post
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.post_item.*
 
-class MainAdapter(var items: List<Post>): RecyclerView.Adapter<MainAdapter.PostHolder>() {
+class MainAdapter(var items: List<Post>): androidx.recyclerview.widget.RecyclerView.Adapter<MainAdapter.PostHolder>() {
     override fun getItemCount() = items.size
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PostHolder(LayoutInflater.from(parent.context).inflate(R.layout.post_item, parent, false))
     override fun onBindViewHolder(holder: PostHolder, position: Int) { holder.bind(items[position]) }
 
-    inner class PostHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer {
+    inner class PostHolder(override val containerView: View): androidx.recyclerview.widget.RecyclerView.ViewHolder(containerView), LayoutContainer {
         fun bind(item: Post) {
             postId.text     = item.id.toString()
             postTitle.text  = item.title
