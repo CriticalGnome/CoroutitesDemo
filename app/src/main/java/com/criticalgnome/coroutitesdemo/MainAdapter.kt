1ac2da9df3c8c3c066dfe2aaf5abf5c3cb87cd40
Sergey Kalinovsky <SergeyKalinovsky@coherentsolutions.com>
2018-09-28 16:18:26 +0300

    Refactored

--- app/src/main/java/com/criticalgnome/coroutitesdemo/MainAdapter.kt
+++ app/src/main/java/com/criticalgnome/coroutitesdemo/MainAdapter.kt
@@ -1,6 +1,5 @@
 package com.criticalgnome.coroutitesdemo
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
